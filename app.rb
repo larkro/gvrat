d6fd3db5b007b3c8e0a819f1df5e5939910f0d98
Lars Kronfält <lakr@mccc.se>
2024-12-08 05:56:36 +0100

    chore: make env page look nice

--- app.rb
+++ app.rb
@@ -51,13 +51,13 @@ get "/env" do
 
   html = <<~HTML
     <h1>Environment Variables</h1>
-    <ul>
+    <pre>
       #{env_vars.filter_map { |key, value| "<li><strong>#{key}:</strong> #{value}</li>" if value }.join}
-    </ul>
+    </pre>
   HTML
 
   status 200
-  body html
+  erb html
 end
 
 post "/" do
